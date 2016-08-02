c4a53eee83e37740acc8ce665b6a12587072ca94
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-02 12:22:29 -0600

    Username, full name, and game types rendering to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var bookshelf = require('../db/bookshelf')
+var bookshelf = require('../db/bookshelf');
+var Dash = require('../lib/dashlogic');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+// probably place all routes above /:username route, no other route files
+
+router.get('/:username', function(req, res, next){
+  Dash.readUser(req.params.username).then(function(user){
+    console.log(user.rows[0].id);
+    Dash.readGameTypes(user.rows[0].id).then(function(gametypes){
+    //   console.log(gametypes);
+    //   res.render('testdash', {
+    //     userInfo: user.rows[0],
+    //     games: gametypes.rows
+    // })
+    // for each game type, we need to get:
+    // 1) Player data
+    // 2) Game Records
+    // 3) Standings
+    for(var i=0; i<gametypes.rows.length; i++){
+      Dash.readGameStats(gametypes.rows[i].id)
+
+    }
+});
+})});
 
 module.exports = router;
