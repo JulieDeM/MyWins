e9a0aae5cd1f673d16ef8fac20db222c3842416c
Julie DeMasters <juliekdemasters@gmail.com>
2016-08-06 13:29:06 -0600

    inserting into the database the team game

--- routes/newgametypes.js
+++ routes/newgametypes.js
@@ -6,14 +6,18 @@ var One = require('../lib/one_v_one');
 var queries = require('../lib/queries');
 
 //posting individual game form to database
-router.post('/', function(req, res, next){
+router.post('/:username', function(req, res, next){
   Dash.createGameType(req.body.userid, req.body.sport).then(function(){
       res.redirect('/:username')
     })
 })
+
 //posting team game form to database
-// router.post('/', function(req,res,next){
-// })
+router.post('/:username', function(req,res,next){
+  Dash.createGameTypeTeam(req.params.username).then(function(){
+    res.redirect('/:username')
+  })
+})
 //route was added to read the game standings by desc
 router.get('/:username', function(req, res, next){
   Dash.readUser(req.params.username).then(function(user){
@@ -21,7 +25,8 @@ router.get('/:username', function(req, res, next){
       Dash.readGameStats(user.rows[0].id).then(function(all){
         Dash.readGameRecords(user.rows[0].id).then(function(records){
           Dash.readGameStandings(user.rows[0].id).then(function(standing){
-            res.render('gamestandings', {
+            //this should be teamplay
+            res.render('teamplayersaddgametodashboard', {
               userInfo: user.rows[0],
               gameTypes: gametypes.rows,
               gameStats: all.rows,
