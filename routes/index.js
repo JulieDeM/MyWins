b795596ac2684b7ce58d336a33d3e78dd321f063
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-06 12:15:39 -0600

    Team names, games, and ratings rendering correctly on dashboard

--- routes/index.js
+++ routes/index.js
@@ -38,13 +38,14 @@ router.get('/:username', function(req, res, next){
           // ALL TEAM functionality below
           Dash.readTeams(user.rows[0].id).then(function(teams){
             console.log(teams.rows);
-            
+
             // RENDER happens in last call
             res.render('testdash', {
               userInfo: user.rows[0],
               gameTypes: gametypes.rows,
               gameStats: all.rows,
-              gameRecords: records.rows
+              gameRecords: records.rows,
+              teamStuff: teams.rows
 
           })
 
@@ -110,6 +111,63 @@ router.post('/addrecord', function(req, res, next){
       })
     })
   })
+})
+
+router.post('/addteamrecord', function(req, res, next){
+  console.log(">>>>>>>>>>>> req body <<<<<<<<<<<<");
+  console.log(req.body);
+  // Dash.createGameRecord(
+  //   req.body.game_id, req.body.user1_id, req.body.user2_id, req.body.user1_score, req.body.user2_score
+  // ).then(function(){
+  //   //update players here
+  //   Dash.readMoreGameStats(req.body.user1_id, req.body.game_id).then(function(results1){
+  //     Dash.readMoreGameStats(req.body.user2_id, req.body.game_id).then(function(results2){
+  //       // console.log("-------------ONE---------");
+  //       // console.log(results1.rows);
+  //       // console.log("-------------TWO---------");
+  //       // console.log(results2.rows);
+  //       var user1 = results1.rows[0];
+  //       var user2 = results2.rows[0];
+  //       var u1ExScore = One.expectedScore(user1.rating, user2.rating);
+  //       var u2ExScore = 1 - u1ExScore;
+  //       if (req.body.user1_score > req.body.user2_score){
+  //         var u1AcScore = 1;
+  //         var u2AcScore = 0;
+  //       } else if (req.body.user1_score < req.body.user2_score){
+  //         var u1AcScore = 0;
+  //         var u2AcScore = 1;
+  //       } else {
+  //         var u1AcScore = 0.5;
+  //         var u2AcScore = 0.5;
+  //       }
+  //       var u1newRating = One.newRating(user1.rating, user1.constant, u1ExScore, u1AcScore);
+  //       var u2newRating = One.newRating(user2.rating, user2.constant, u2ExScore, u2AcScore);
+  //       var updateU1 = {
+  //         gamesPlayed: user1.gamesPlayed + 1,
+  //         rating: u1newRating,
+  //         constant: One.checkConstant(user1.constant, u1newRating, (user1.gamesPlayed + 1))
+  //       };
+  //       var updateU2 = {
+  //         gamesPlayed: user2.gamesPlayed + 1,
+  //         rating: u2newRating,
+  //         constant: One.checkConstant(user2.constant, u2newRating, (user2.gamesPlayed + 1))
+  //       };
+  //       // console.log("-------------ONE---------");
+  //       // console.log(updateU1);
+  //       // console.log("-------------TWO---------");
+  //       // console.log(updateU2);
+  //       Dash.updatePlayer(updateU1, user1.user_game_id).then(function(){
+  //         Dash.updatePlayer(updateU2, user2.user_game_id).then(function(){
+  //           res.redirect('/')
+  //
+  //         })
+  //       })
+  //
+  //
+  //
+  //     })
+  //   })
+  // })
 })
 
 module.exports = router;
