fa0f5de888b9ee988ac49c7009f1a217f9d0dc30
Ricky Padilla <rickycpadilla@gmail.com>
2016-08-07 12:00:32 -0600

    ratings now working flawlessly for one v one

--- routes/index.js
+++ routes/index.js
@@ -21,33 +21,12 @@ router.get('/', function(req, res, next) {
 router.get('/:username', function(req, res, next){
   Dash.readUser(req.params.username).then(function(user){
     Dash.readGameTypes(user.rows[0].id).then(function(gametypes){
-      // console.log("************GAME TYPES**********");
-      // console.log(gametypes.rows);
-
-    // for each game type, we need to get:
-    // 1) Player data
-    // 2) Game Records
-    // 3) Standings
       Dash.readGameStats(user.rows[0].id).then(function(all){
-        // console.log("************GAME STATS**********");
-        // console.log(all.rows);
         Dash.readGameRecords(user.rows[0].id).then(function(records){
-          // console.log("************GAME RECORDS**********");
-          // console.log(records.rows);
-
-          // ALL TEAM functionality below
           Dash.readTeams(user.rows[0].id).then(function(teams){
-            // console.log(">>>>>>>>>>>teams<<<<<<<<<<<<");
-            // console.log(teams.rows);
             Dash.readTeamRecords(user.rows[0].id).then(function(teamrecords){
-              // console.log("***************RECORDS************");
-              // console.log(teamrecords.rows);
               Dash.readAllTeamNames().then(function(allTeams){
-                // console.log("------------- ALL TEAMS --------------");
-                // console.log(allTeams.rows);
                 Dash.readAllUsers().then(function(allUsers){
-                  // RENDER happens in last call
-                  console.log(allUsers.rows);
                   res.render('testdash', {
                     userInfo: user.rows[0],
                     gameTypes: gametypes.rows,
@@ -58,18 +37,13 @@ router.get('/:username', function(req, res, next){
                     teamRecords: teamrecords.rows,
                     allTeams: allTeams.rows
                   })
-
                 })
-
               })
-
-          })
-
-
+            })
           })
         })
       })
-});
+    });
 })});
 
 router.post('/addrecord', function(req, res, next){
@@ -81,26 +55,37 @@ router.post('/addrecord', function(req, res, next){
     //update players here
     Dash.readMoreGameStats(req.body.user1_id, req.body.game_id).then(function(results1){
       Dash.readMoreGameStats(req.body.user2_id, req.body.game_id).then(function(results2){
-        // console.log("-------------ONE---------");
-        // console.log(results1.rows);
-        // console.log("-------------TWO---------");
-        // console.log(results2.rows);
+        console.log("-------------ONE---------");
+        console.log(results1.rows);
+        console.log("-------------TWO---------");
+        console.log(results2.rows);
         var user1 = results1.rows[0];
         var user2 = results2.rows[0];
         var u1ExScore = One.expectedScore(user1.rating, user2.rating);
         var u2ExScore = 1 - u1ExScore;
-        if (req.body.user1_score > req.body.user2_score){
+        console.log(u1ExScore);
+        console.log(u2ExScore);
+        console.log("============= scores ============");
+        console.log(req.body.user1_score);
+        console.log(req.body.user2_score);
+        if (Number(req.body.user1_score) > Number(req.body.user2_score)){
           var u1AcScore = 1;
           var u2AcScore = 0;
-        } else if (req.body.user1_score < req.body.user2_score){
+        } else if (Number(req.body.user1_score) < Number(req.body.user2_score)){
           var u1AcScore = 0;
           var u2AcScore = 1;
         } else {
           var u1AcScore = 0.5;
           var u2AcScore = 0.5;
         }
+        console.log("+++++++++++ AcScores ++++++++++");
+        console.log(u1AcScore);
+        console.log(u2AcScore);
         var u1newRating = One.newRating(user1.rating, user1.constant, u1ExScore, u1AcScore);
         var u2newRating = One.newRating(user2.rating, user2.constant, u2ExScore, u2AcScore);
+        console.log("********* new ratings **********");
+        console.log(u1newRating);
+        console.log(u2newRating);
         var updateU1 = {
           gamesPlayed: user1.gamesPlayed + 1,
           rating: u1newRating,
@@ -111,13 +96,13 @@ router.post('/addrecord', function(req, res, next){
           rating: u2newRating,
           constant: One.checkConstant(user2.constant, u2newRating, (user2.gamesPlayed + 1))
         };
-        // console.log("-------------ONE---------");
-        // console.log(updateU1);
-        // console.log("-------------TWO---------");
-        // console.log(updateU2);
+        console.log("-------------UPDATED ONE---------");
+        console.log(updateU1);
+        console.log("-------------UPDATED TWO---------");
+        console.log(updateU2);
         Dash.updatePlayer(updateU1, user1.user_game_id).then(function(){
           Dash.updatePlayer(updateU2, user2.user_game_id).then(function(){
-            res.redirect('/')
+            res.redirect('/bestplayer')
 
           })
         })
