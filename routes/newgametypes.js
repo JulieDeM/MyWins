31dc084bfb4ff2a3f419efce09651eab03f9706b
Julie DeMasters <juliekdemasters@gmail.com>
2016-08-07 12:34:29 -0600

    routes were created on the newgametype route to try to avoid conflicts

--- routes/newgametypes.js
+++ routes/newgametypes.js
@@ -6,16 +6,18 @@ var One = require('../lib/one_v_one');
 var queries = require('../lib/queries');
 
 //posting individual game form to database
-router.post('/:username', function(req, res, next){
-  Dash.createGameType(req.params.username, req.body.userid, req.body.sport).then(function(){
-      res.redirect('/:username')
-    })
-})
+// router.post('/:username', function(req, res, next){
+//   Dash.createGameType(req.params.username, req.body.userid, req.body.sport).then(function(){
+//       res.redirect('/:username')
+//     })
+// })
 
 //posting team game form to database
 router.post('/:username', function(req,res,next){
-  Dash.createGameTypeTeam(req.params.username).then(function(){
-    res.redirect('/:username')
+  Dash.createGameType(req.params.username, req.body.userid, req.body.sport).then(function(){
+    Dash.createGameTypeTeam(req.params.username, req.body.userid, req.body.sport).then(function(){
+      res.redirect('/:username')
+    })
   })
 })
 //route was added to read the game standings by desc
