420771fd10ce33d31333dbef8787d49717cafb55
Julie DeMasters <juliekdemasters@gmail.com>
2016-08-07 20:51:47 -0600

    trying to commit the final index.js

--- routes/index.js
+++ routes/index.js
@@ -1,184 +1,189 @@
 var express = require('express');
 var router = express.Router();
-var bookshelf = require('../db/bookshelf');
 var Dash = require('../lib/dashlogic');
 var One = require('../lib/one_v_one');
+var Two = require('../lib/two_v_two');
+var Edit = require('../lib/editlogic');
 var Dashboard = require('../lib/queries')
 var Val = require('../lib/formValidation')
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('splashpage');
+    res.render('splashpage');
 });
-
-router.post('/validate', function(req, res, next){
-  console.log(req.body);
-  var validate = Val.validateUsername(req.body)
-  console.log("***VALIDATE***");
-  console.log(validate);
-  switch(validate){
-    case "blank":
-      res.redirect('/noUserName');
-      console.log("no username");
-      // console.log(message);
-      break;
-    case "illegal":
-      res.redirect('/invalidUserName');
-      console.log("illegalasdkugajsdjhasbd");
-      // console.log(message);
-      break;
-    case "nosport":
-      console.log("add sport");
-      res.redirect('/noSport')
-    case "no error":
-    console.log("no error");
-      Dashboard.findUser(req.body.name).then(function(result){
-        if (result.rows.length == 0) {
-          //find user ID based on FB id in cookies
-          Dashboard.findUserId(req.cookies.user).then(function(userID){
-            //if not, add to users table in database
-            Dashboard.updateUser(req.body.name, req.body.sport, req.cookies.user).then(function(){
-                //then use that Id and the game id from req.body to add user game record
-                Dashboard.addUserGame(userID.rows[0].id, req.body.sport).then(function(){
-                  //then find userGameId of record just added
-                  Dashboard.findUserGameId(userID.rows[0].id).then(function(userGameId){
-                    //then add a userGameStats record
-                    Dashboard.addUserGameStats(userID.rows[0].id, userGameId.rows[0].id, 0, 100, 40).then(function(){
+router.post('/validate', function(req, res, next) {
+    var validate = Val.validateUsername(req.body)
+    switch (validate) {
+        case "blank":
+            res.redirect('/noUserName');
+            break;
+        case "illegal":
+            res.redirect('/invalidUserName');
+            break;
+        case "nosport":
+            res.redirect('/noSport')
+        case "no error":
+            Dashboard.findUser(req.body.name).then(function(result) {
+                if (result.rows.length == 0) {
+                    //find user ID based on FB id in cookies
+                    Dashboard.findUserId(req.cookies.user).then(function(userID) {
+                        //if not, add to users table in database
+                        Dashboard.updateUser(req.body.name, req.body.sport, req.cookies.user).then(function() {
+                            //then use that Id and the game id from req.body to add user game record
+                            Dashboard.addUserGame(userID.rows[0].id, req.body.sport).then(function() {
+                                //then find userGameId of record just added
+                                Dashboard.findUserGameId(userID.rows[0].id).then(function(userGameId) {
+                                    //then add a userGameStats record
+                                    Dashboard.addUserGameStats(userID.rows[0].id, userGameId.rows[0].id, 0, 100, 40).then(function() {})
+                                })
+                            })
+                        })
+                        res.redirect('/loading')
                     })
-                  })
+                } else {
+                    console.log("That username has been taken!");
+                    res.redirect('/takenUserName')
+                }
+            })
+            break;
+    }
+});
+router.get('/noUserName', function(req, res, next) {
+    res.render('pageafterfb', {
+        message: "Please enter a username!"
+    })
+})
+router.get('/takenUserName', function(req, res, next) {
+    res.render('pageafterfb', {
+        message: "Sorry, this username has been taken!"
+    })
+})
+router.get('/invalidUserName', function(req, res, next) {
+    res.render('pageafterfb', {
+        message: "Username can only contain lowercase letters, numbers, and underscores."
+    })
+})
+router.get('/noSport', function(req, res, next) {
+    res.render('pageafterfb', {
+        message: "Please choose a sport!"
+    })
+})
+router.get('/loading', function(req, res, next) {
+    Dashboard.readUser(req.cookies.user).then(function(output) {
+        var info = output.rows[0]
+            // arr.push(info)
+        var photo = info.image_url.replace('$1', '?');
+        // console.log(output);
+        res.render('loadpage', {
+            photo: photo,
+            firstName: info.firstName,
+            lastName: info.lastName,
+            userName: info.userName
+        });
+        // setTimeout(function(){res.redirect("/demjules")}, 1000)
+    })
+});
+//posting team game form to database
+router.post('/:username', function(req, res, next) {
+        Dash.createGameType(req.params.username, req.body.userid, req.body.sport).then(function() {
+            Dash.createGameTypeTeam(req.params.username, req.body.userid, req.body.sport).then(function() {
+                res.redirect('/:username')
+            })
+        })
+    })
+    //change favorite game
+router.post('/:id/:gameid', function(req, res, next) {
+        Edit.changeFavorite(req.params.id, req.params.gameid).then(function() {
+            Edit.getUserName(req.cookies.user).then(function(user) {
+                res.redirect(`/${user.rows[0].userName}`)
+            })
+        })
+    })
+    // Ricky's work below
+router.get('/:username', function(req, res, next) {
+    var currUserID = req.cookies.user;
+    Dash.readUser(req.params.username).then(function(user) {
+        Dash.readGameTypes(user.rows[0].id).then(function(gametypes) {
+            Dash.readGameStats(user.rows[0].id).then(function(all) {
+                Dash.readGameRecords(user.rows[0].id).then(function(records) {
+                    Dash.readTeams(user.rows[0].id).then(function(teams) {
+                        Dash.readTeamRecords(user.rows[0].id).then(function(teamrecords) {
+                            Dash.readAllTeamNames().then(function(allTeams) {
+                                Dash.readAllUsers().then(function(allUsers) {
+                                    res.render('dashboard_final', {
+                                        currUserID: currUserID,
+                                        faveGame: user.rows[0].favorite_game_id,
+                                        userInfo: user.rows[0],
+                                        gameTypes: gametypes.rows,
+                                        gameStats: all.rows,
+                                        gameRecords: records.rows,
+                                        allUsers: allUsers.rows,
+                                        teamStuff: teams.rows,
+                                        teamRecords: teamrecords.rows,
+                                        allTeams: allTeams.rows
+                                    })
+                                })
+                            })
+                        })
+                    })
                 })
             })
-            res.redirect('/loading')
-          })
-        }else{
-          console.log("That username has been taken!");
-          res.redirect('/takenUserName')
-        }
-      })
-        break;
-    }
-  });
-
-router.get('/noUserName', function(req,res,next){
-  res.render('pageafterfb', {message: "Please enter a username!"})
-})
-router.get('/takenUserName', function(req,res,next){
-  res.render('pageafterfb', {message: "Sorry, this username has been taken!"})
-})
-
-router.get('/invalidUserName', function(req,res,next){
-  res.render('pageafterfb', {message: "Username can only contain lowercase letters, numbers, and underscores."})
-})
-router.get('/noSport', function(req,res,next){
-  res.render('pageafterfb', {message: "Please choose a sport!"})
-})
-
-router.get('/loading', function(req,res,next){
-  Dashboard.readUser(req.cookies.user).then(function(output){
-    var info = output.rows[0]
-    // arr.push(info)
-    var photo = info.image_url.replace('$1', '?');
-    // console.log(output);
-    res.render('loadpage', {photo : photo, firstName : info.firstName, lastName : info.lastName, userName : info.userName});
-    // setTimeout(function(){res.redirect("/demjules")}, 1000)
-  })
-});
-
-// probably place all routes above /:username route, no other route files
-
-//Julie's code ends here
-//==========code ends here============
-
-// Ricky's work below
-router.get('/:username', function(req, res, next){
-  Dash.readUser(req.params.username).then(function(user){
-    console.log(req.params.username);
-    Dash.readGameTypes(user.rows[0].id).then(function(gametypes){
-      // console.log("************GAME TYPES**********");
-      // console.log(gametypes.rows);
-    //   res.render('testdash', {
-    //     userInfo: user.rows[0],
-    //     games: gametypes.rows
-    // })
-    // for each game type, we need to get:
-    // 1) Player data
-    // 2) Game Records
-    // 3) Standings
-      Dash.readGameStats(user.rows[0].id).then(function(all){
-        // console.log("************GAME STATS**********");
-        // console.log(all.rows);
-        Dash.readGameRecords(user.rows[0].id).then(function(records){
-          // console.log("************GAME RECORDS**********");
-          // console.log(records.rows);
-          //this needs to be changed back to testdash i changed it to test to the long one
-            // res.render('teamplayersaddgametodashboard', {
-            res.render('dash', {
-              userInfo: user.rows[0],
-              gameTypes: gametypes.rows,
-              gameStats: all.rows,
-              gameRecords: records.rows
-          })
-        })
-      })
-});
-})});
-
-router.post('/addrecord', function(req, res, next){
-  // console.log(">>>>>>>>>>>> req body <<<<<<<<<<<<");
-  // console.log(req.body);
-  Dash.createGameRecord(
-    req.body.game_id, req.body.user1_id, req.body.user2_id, req.body.user1_score, req.body.user2_score
-  ).then(function(){
-    //update players here
-    Dash.readMoreGameStats(req.body.user1_id, req.body.game_id).then(function(results1){
-      Dash.readMoreGameStats(req.body.user2_id, req.body.game_id).then(function(results2){
-        console.log("-------------ONE---------");
-        console.log(results1.rows);
-        console.log("-------------TWO---------");
-        console.log(results2.rows);
-        // results not getting me the game type, just user_game_id
-        var user1 = results1.rows[0];
-        var user2 = results2.rows[0];
-        var u1ExScore = One.expectedScore(user1.rating, user2.rating);
-        var u2ExScore = 1 - u1ExScore;
-        if (req.body.user1_score > req.body.user2_score){
-          var u1AcScore = 1;
-          var u2AcScore = 0;
-        } else if (req.body.user1_score < req.body.user2_score){
-          var u1AcScore = 0;
-          var u2AcScore = 1;
-        } else {
-          var u1AcScore = 0.5;
-          var u2AcScore = 0.5;
-        }
-        var u1newRating = One.newRating(user1.rating, user1.constant, u1ExScore, u1AcScore);
-        var u2newRating = One.newRating(user2.rating, user2.constant, u2ExScore, u2AcScore);
-        var updateU1 = {
-          gamesPlayed: user1.gamesPlayed + 1,
-          rating: u1newRating,
-          constant: One.checkConstant(user1.constant, u1newRating, (user1.gamesPlayed + 1))
-        };
-        var updateU2 = {
-          gamesPlayed: user2.gamesPlayed + 1,
-          rating: u2newRating,
-          constant: One.checkConstant(user2.constant, u2newRating, (user2.gamesPlayed + 1))
-        };
-        // console.log("-------------ONE---------");
-        // console.log(updateU1);
-        // console.log("-------------TWO---------");
-        // console.log(updateU2);
-        Dash.updatePlayer(updateU1, user1.user_game_id).then(function(){
-          Dash.updatePlayer(updateU2, user2.user_game_id).then(function(){
-            res.redirect('/')
-
-          })
-        })
-
-
-
-      })
+        });
     })
-  })
-})
-
+});
+router.post('/addrecord', function(req, res, next) {
+    Dash.createGameRecord(
+        req.body.game_id, req.body.user1_id, req.body.user2_id, req.body.user1_score, req.body.user2_score
+    ).then(function() {
+        Dash.readMoreGameStats(req.body.user1_id, req.body.game_id).then(function(results1) {
+            Dash.readMoreGameStats(req.body.user2_id, req.body.game_id).then(function(results2) {
+                var user1 = results1.rows[0];
+                var user2 = results2.rows[0];
+                if (results2.rows.length !== 0) {
+                    var updatedPlayers = One.runMath(user1, user2, req.body.user1_score, req.body.user2_score)
+                    Dash.updatePlayer(updatedPlayers[0], user1.user_game_id).then(function() {
+                        Dash.updatePlayer(updatedPlayers[1], user2.user_game_id).then(function() {
+                            Dash.readUserId(req.body.user1_id).then(function(userStuff) {
+                                var userName = userStuff.rows[0].userName;
+                                res.redirect('/' + userName)
+                            })
+                        })
+                    })
+                } else {
+                    //add user_id and game_id to user_games table
+                    Dash.createPlayerGame(req.body.user2_id, req.body.game_id).then(function() {
+                        //get the user_game_id from the user_games table as 'result'
+                        Dash.UserGameId(req.body.user2_id).then(function(result) {
+                            //insert a record for user2 into the user_game_stats table
+                            Dash.addStats(req.body.user2_id, result.rows[0].id).then(function() {
+                                Dash.PlayerName(req.body.user1_id).then(function(player) {
+                                    res.redirect(`/${player.rows[0].userName}`)
+                                })
+                            })
+                        })
+                    })
+                }
+            })
+        })
+    })
+});
+router.post('/addteamrecord', function(req, res, next) {
+    Dash.createGameRecordTeam(
+        req.body.game_id, req.body.team1_id, req.body.team2_id, req.body.team1_score, req.body.team2_score
+    ).then(function() {
+        Dash.readTeamStats(req.body.team1_id, req.body.game_id).then(function(results1) {
+            Dash.readTeamStats(req.body.team2_id, req.body.game_id).then(function(results2) {
+                var team1 = results1.rows[0];
+                var team2 = results2.rows[0];
+                var updatedTeams = Two.runMath(team1, team2, req.body.team1_score, req.body.team2_score);
+                Dash.updateTeam(updatedTeams[0], team1.team_game_id).then(function() {
+                    Dash.updateTeam(updatedTeams[1], team2.team_game_id).then(function() {
+                        Dash.readTeamId(req.body.team1_id).then(function(teamStuff) {
+                            res.redirect('/' + teamStuff.rows[0].name)
+                        })
+                    })
+                })
+            })
+        });
+    })
+});
 module.exports = router;
